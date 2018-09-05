3d4016878b13da4c1d20d7713055b272ae8b1df0
Justina Petraityte <juste@rasa.com>
2018-09-05 11:40:19 +0200

    changed api

--- actions.py
+++ actions.py
@@ -19,7 +19,7 @@ class ActionJoke(Action):
 
     def run(self, dispatcher, tracker, domain):
         # what your action should do
-        request = json.loads(requests.get('https://api.chucknorris.io/jokes/random').text) #make an apie call
-        joke = request['value'] #extract a joke from returned json response
+        request = requests.get('http://api.icndb.com/jokes/random').json() #make an apie call
+        joke = request['value']['joke'] #extract a joke from returned json response
         dispatcher.utter_message(joke) #send the message back to the user
         return []
