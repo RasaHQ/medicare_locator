7520ec5cb10b4a3e43255b09d2b30c0c553a15f8
EPedrotti <e.pedrotti@rasa.com>
2018-11-27 11:45:10 +0100

    added stories for treatment center

--- actions.py
+++ actions.py
@@ -92,3 +92,81 @@ class HospitalForm(FormAction):
         # utter submit template
         dispatcher.utter_template('utter_submit', tracker)
         return []
+
+
+class CenterForm(FormAction):
+    """Example of a custom form action"""
+
+    def name(self):
+        # type: () -> Text
+        """Unique identifier of the form"""
+
+        return "center_form"
+
+    @staticmethod
+    def required_slots(tracker):
+        # type: (Tracker) -> List[Text]
+        """A list of required slots that the form has to fill"""
+
+        return ["zip", "type"]
+
+    def slot_mappings(self):
+        # type: () -> Dict[Text: Union[Dict, List[Dict]]]
+        return {"zip": self.from_entity(entity="number"),
+                "type": self.from_entity(entity="type")}
+
+    @staticmethod
+    def is_zip(string):
+        # type: (Text) -> bool
+        """Check if a string is an integer"""
+        try:
+            int(string)
+            return len(string) == 5
+        except ValueError:
+            return False
+
+    def validate(self, dispatcher, tracker, domain):
+        # type: (CollectingDispatcher, Tracker, Dict[Text, Any]) -> List[Dict]
+        """Validate extracted requested slot
+            else reject the execution of the form action
+        """
+        # extract other slots that were not requested
+        # but set by corresponding entity
+        slot_values = self.extract_other_slots(dispatcher, tracker, domain)
+
+        # extract requested slot
+        slot_to_fill = tracker.get_slot(REQUESTED_SLOT)
+        if slot_to_fill:
+            slot_values.update(self.extract_requested_slot(dispatcher,
+                                                           tracker, domain))
+            if not slot_values:
+                # reject form action execution
+                # if some slot was requested but nothing was extracted
+                # it will allow other policies to predict another action
+                raise ActionExecutionRejection(self.name(),
+                                               "Failed to validate slot {0} "
+                                               "with action {1}"
+                                               "".format(slot_to_fill,
+                                                         self.name()))
+
+        # we'll check when validation failed in order
+        # to add appropriate utterances
+        for slot, value in slot_values.items():
+            if slot == 'zip':
+                if not self.is_zip(value):
+                    dispatcher.utter_template('utter_wrong_num_people',
+                                              tracker)
+                    # validation failed, set slot to None
+                    slot_values[slot] = None
+
+        # validation succeed, set the slots values to the extracted values
+        return [SlotSet(slot, value) for slot, value in slot_values.items()]
+
+    def submit(self, dispatcher, tracker, domain):
+        # type: (CollectingDispatcher, Tracker, Dict[Text, Any]) -> List[Dict]
+        """Define what the form has to do
+            after all required slots are filled"""
+
+        # utter submit template
+        dispatcher.utter_template('utter_submit', tracker)
+        return []
